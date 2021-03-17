cb426a7db500c84029e6a6be4c10b8264f476eda
terrorRuntime <kishorbundhe@gmail.com>
2021-03-17 21:12:50 +0530

    Demonstrating use of component Scan

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,15 +1,18 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
-/**
- * Created by jt on 12/28/19.
- */
+import guru.springframework.pets.PetService;
+
+@Controller
 public class PetController {
-
-    private final PetService petService;
-
-    public String whichPetIsTheBest(){
+	private final PetService petService;
+	
+	public PetController(PetService petService) {
+		this.petService = petService;
+	}
+	public String whichPetIsTheBest(){
         return petService.getPetType();
     }
 }
